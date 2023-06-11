cad65a88edcb1cb0ee37b3ff5122741e210dc521
spyroot <spyroot@gmail.com>
2023-06-12 03:07:11 +0400

    fixed with trainer

--- igc/modules/igc_train_auto_state_encoder.py
+++ igc/modules/igc_train_auto_state_encoder.py
@@ -3,13 +3,13 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
 
+from torch.utils.data import DataLoader
 from igc.ds.redfish_dataset import JSONDataset
-from igc.shared.shared_torch_builder import TorchBuilder
 from .base.igc_base_module import IgcBaseModule
 from .base.igc_metric_logger import MetricLogger
 from igc.modules.llm.igc_autoencoder import AutoStateEncoder
+from igc.shared.shared_torch_builder import TorchBuilder
 import torch.nn.functional as F
 
 
@@ -19,7 +19,6 @@ class AutoencoderTrainer(IgcBaseModule):
     state dimension of latent space llm outputs.
 
     """
-
     def __init__(self,
                  module_name: str,
                  spec: argparse.Namespace,
@@ -46,22 +45,20 @@ class AutoencoderTrainer(IgcBaseModule):
             metric_logger=metric_logger,
             is_inference=is_inference)
 
-        self._llm_model = llm_model
-        self._input_dim = llm_model.config.hidden_size
-        self._latent_dim = llm_model.config.hidden_size
+        self._input_dim = self.model.config.hidden_size
+        self._latent_dim = self.model.config.hidden_size
         self._learning_rate = spec.auto_encoder_lr
 
         self.logger.info(f"Creating auto-encoder input dim"
                          f" {self._input_dim} {self._latent_dim} batch_size: {self.batch_size}")
 
-        self._encoder_model = llm_model.transformer
+        self._encoder_model = self.model.transformer
         llm_model.transformer.config.is_decoder = False
         # self._llm_model.resize_token_embeddings(len(llm_tokenizer))
         input_shape = self._encoder_model.wpe.weight.shape
         self.emb_shape = (input_shape[0] - 1, input_shape[1])
         self.model_autoencoder = AutoStateEncoder()
 
-        # 0.001
         self.logger.info(f"Creating optimizer {spec.auto_encoder_optimizer} "
                          f"lr: {spec.auto_encoder_lr} "
                          f"weight decay: {spec.auto_encoder_weight_decay}")
@@ -86,25 +83,16 @@ class AutoencoderTrainer(IgcBaseModule):
         x, _ = batch
         x_hat = self.model.forward(x)
         loss = torch.nn.functional.mse_loss(x, x_hat, reduction="none")
-        loss = loss.sum(dim=[1, 2, 3]).mean(dim=[0])
-        return loss
-
-    def _get_reconstruction_loss(self, batch):
-        """Given a batch of images, this function returns the reconstruction loss (MSE in our case)"""
-        x, _ = batch  # We do not need the labels
-        x_hat = self.forward(x)
-        loss = F.mse_loss(x, x_hat, reduction="none")
-        loss = loss.sum(dim=[1, 2, 3]).mean(dim=[0])
         return loss
 
     def encode(self):
         """
         :return:
         """
-        input_dim = self._llm_model.config.hidden_size
+        input_dim = self.model.config.hidden_size
         latent_dim = 128
 
-        gpt_encoder = self._llm_model.get_input_embeddings()
+        gpt_encoder = self.model.get_input_embeddings()
         autoencoder = AutoStateEncoder(input_dim, latent_dim)
 
         # attach the autoencoder to the GPT model
@@ -165,7 +153,6 @@ class AutoencoderTrainer(IgcBaseModule):
             print(f"Epoch [{epoch + 1}/{num_epochs}], Average Loss: {average_loss}")
 
             # Perform validation or evaluation steps if needed
-
         # self.save_model()
         # # Save the trained model if desired
         # torch.save(self.model.state_dict(), "trained_model.pth")
