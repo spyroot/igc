0f37e2b91097415477fc8d8b639131dc37cdbf20
spyroot <spyroot@gmail.com>
2023-06-12 07:59:42 +0400

    fixed with trainer

--- igc/modules/igc_train_auto_state_encoder.py
+++ igc/modules/igc_train_auto_state_encoder.py
@@ -155,7 +155,8 @@ class AutoencoderTrainer(IgcBaseModule):
 
         self.model_autoencoder.train()
         self.model_autoencoder, self.optimizer = self.accelerator.prepare(
-            [train_dataloader, self.model_autoencoder, self.optimizer], [True])
+            [train_dataloader, self.model_autoencoder, self.optimizer],
+            device_placement=[False, True, True])
 
         # self.model_autoencoder.train()
         # self.model_autoencoder.to(self.device)
