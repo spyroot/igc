525935d4312867503b81fe46419933b26b8c6fd7
spyroot <spyroot@gmail.com>
2023-06-20 22:47:01 +0400

    Optimized masking switching

--- accelerate_train.sh
+++ accelerate_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-accelerate launch --config_file ./config.yaml igc_main.py --train llm --num_train_epochs 1000 \
+accelerate launch --config_file ./config.yaml igc_main.py --train llm --num_train_epochs 1000 --use_accelerator \
 	--llm latent --llm_log_level info --log_level info
